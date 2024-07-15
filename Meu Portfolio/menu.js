e679bb3687f76021b46192e86d3541dc652c12e6
EuGabis <pereiragabriel08790@gmail.com>
2024-07-14 21:07:43 -0300

    Add files via upload
    Meu portfolio

---
+++ Meu Portfolio/menu.js
@@ -0,0 +1,99 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+document.getElementById('contact-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    const data = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        phone: formData.get('phone'),
+        message: formData.get('message'),
+        accessKey: '40eda3ad-f7d3-4ca2-a8ed-3a3faad678b0'
+    };
+
+    try {
+        const response = await fetch('https://api.staticforms.xyz/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer SEU_TOKEN_DE_AUTENTICACAO' // Adicione seu token de autenticação aqui
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            window.location.href = 'obrigado.html';
+        } else {
+            const errorData = await response.json();
+            alert('Erro: ' + errorData.message);
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao enviar o formulário.');
+    }
+});
+
+
+$(document).ready(function(){
+    Inputmask({"mask": "99999999999", "placeholder": "11987654321"}).mask("input[name='phone']");
+
+    document.getElementById('contact-form').addEventListener('submit', async function(event) {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        const data = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            phone: formData.get('phone'),
+            message: formData.get('message'),
+            accessKey: '40eda3ad-f7d3-4ca2-a8ed-3a3faad678b0'
+        };
+
+        try {
+            const response = await fetch('https://api.staticforms.xyz/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer SEU_TOKEN_DE_AUTENTICACAO' // Adicione seu token de autenticação aqui
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                window.location.href = 'obrigado.html';
+            } else {
+                const errorData = await response.json();
+                alert('Erro: ' + errorData.message);
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            alert('Ocorreu um erro ao enviar o formulário.');
+        }
+    });
+});
+
+
+
+
+
